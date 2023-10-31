e6950a636fe5ac6e6e1371e166f517b8e62ec987
Gabriel Martínez Matínez <63005933+ghmartinez@users.noreply.github.com>
2023-10-31 08:58:26 +0100

    Changed to trivial display

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './14_router.jsx'
+import App from './11_trivial.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
